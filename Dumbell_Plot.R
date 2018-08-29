2fd043958272e471ed6994d7bc080f326c9f3224
horaciochacon <horacio.chacon.t@upch.pe>
2018-08-28 23:48:37 -0500

    Mejoramiento de los comentarios

--- Dumbell_Plot.R
+++ Dumbell_Plot.R
@@ -2,11 +2,13 @@ library(ggplot2) # devtools::install_github("hadley/ggplot2")
 library(ggalt)   # devtools::install_github("hrbrmstr/ggalt")
 library(dplyr)   # Para poder usar data_frame() & arrange()
 
-# Se puede usar directamente una data frame 
+# Se puede usar directamente un data frame creado previamente con las columnas necesarias:
+# En este caso 1 columna categórica, 2 numéricas (definen el diferencial) y el difrencial como STRING
+
 df <- data_frame(Departamento=c("Amazonas", "Ancash", "Apurimac", "Arequipa", "Ayacucho", "Cajamarca",
                            "Callao", "Cusco", "Huancavelica", "Huanuco", "Ica", "Junin",
                            "La Libertad", "Lambayeque", "Lima", "Loreto", "Madre de Dios", "Moquegua",
-                           "Pasco", "Piura", "Puno", "San Mart??n", "Tacna", "Tumbes", "Ucayali"),
+                           "Pasco", "Piura", "Puno", "San Martín", "Tacna", "Tumbes", "Ucayali"),
                  JNC=c(0.158, 0.140, 0.120, 0.0930, 0.120, 0.171,
                            0.169, 0.0900, 0.0960, 0.0870, 0.163, 0.100,
                            0.119, 0.104, 0.177, 0.134, 0.0910, 0.142,
@@ -15,16 +17,14 @@ df <- data_frame(Departamento=c("Amazonas", "Ancash", "Apurimac", "Arequipa", "A
                                  0.358, 0.233, 0.208, 0.211, 0.363, 0.294,
                                  0.270, 0.291, 0.381, 0.308, 0.260, 0.320,
                                  0.256, 0.319, 0.286, 0.286, 0.391, 0.299, 0.224),
-                 diff= paste("+",sprintf("%.1f", (AHA-JNC)*100))
+                 diff= paste("+",sprintf("%.1f", (AHA-JNC)*100)) # Crea un STRING calculado del diferencial con formato de 1 decimal (%.1f)
 )
 
-# we want to keep the order in the plot, so we use a factor for Departamento
+df$Departamento <- factor(df$Departamento, levels=rev(df$Departamento)) 
 
-df$Departamento <- factor(df$Departamento, levels=rev(df$Departamento))
-
-
-# we only want the first line values with "%" symbols (to avoid chart junk)
-# quick hack; there is a more efficient way to do this
+# Función que recibe un vector y convierte  el primer elemento a string con el símbolo % y el resto
+# queda como solo número (visualmente porque en realidad sigue siendo un string, dado el sprintf)
+# esto hace que las etiquetas numéricas solo tenga en la primera fila el símbolo de porcentaje.
 percent_first <- function(x) {
   x <- sprintf("%.1f%%", round(x*100,1))
   x[2:length(x)] <- sub("%$", "", x[2:length(x)])
@@ -32,9 +32,12 @@ percent_first <- function(x) {
 }
 
 gg <- ggplot()
-# doing this vs y axis major grid line
+
+# Crea eje XY con las categoría en Y y los valores en X desde los valores 0 hasta 0.45, el tipo de gráfico
 gg <- gg + geom_segment(data=df, aes(y=Departamento, yend=Departamento, x=0, xend=0.45), color="#b2b2b2", size=0.15)
-# dum???dum???dum!bell
+
+# Superpone el gráfico tipo dumbell con un valor x asi como un valor xend (el primer valor y el valor posterior)
+# se define el grosor de la línea "size", el tamaño de "x" y "xend" así como sus respectivos colores
 gg <- gg + geom_dumbbell(data=df, aes(y=Departamento, x=JNC, xend=AHA),
                          size=1.5, color="#b2b2b2", size_x = 5, size_xend = 5, colour_x ="#9fb059", colour_xend ="#edae52")
 # text below points
@@ -56,8 +59,8 @@ gg <- gg + geom_text(data=filter(df, Departamento=="Amazonas"), aes(x=0.55, y=De
                      color="#7a7d7e", size=3.1, vjust=-2, fontface="bold", family="Calibri")
 gg <- gg + scale_x_continuous(expand=c(0,0), limits=c(0, 0.65))
 gg <- gg + scale_y_discrete(expand=c(0.075,0))
-gg <- gg + labs(x=NULL, y=NULL, title="T??tulo",
-                subtitle="Subt??tulo")
+gg <- gg + labs(x=NULL, y=NULL, title="Título",
+                subtitle="Subtítulo")
 gg <- gg + theme_bw(base_family="Calibri")
 gg <- gg + theme(panel.grid.major=element_blank())
 gg <- gg + theme(panel.grid.minor=element_blank())
